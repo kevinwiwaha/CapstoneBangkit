affd5b23d0b60cd5d7b6e677dc766d00cae342b9
kevinwiwaha <kevinwiwaha@gmail.com>
2021-06-03 13:09:30 +0700

    update server with base64 decode

--- MachineLearning/Server/main.py
+++ MachineLearning/Server/main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import os
 import json
 import numpy as np
+import base64
 import matplotlib.pyplot as plt
 
 
@@ -17,16 +18,35 @@ def hello_world():
 
 @app.route("/predict", methods=['POST'])
 def predict():
-    file = '../Model/images/laptop.jpg'
-    img = tf.keras.preprocessing.image.load_img(file, target_size=(224, 224))
+    # file = '../Model/images/laptop.jpg'
+
+    image = generate_image_from_base64(
+        request.form["title"], request.form["body"])
+    img = tf.keras.preprocessing.image.load_img(image, target_size=(224, 224))
     x = tf.keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     images = np.vstack([x])
     history = model.predict(images)
     print(history[0][0])
+    os.remove(get_filename(request.form["title"]))
     return {
-        "prediction": str(history[0][0])
+        "userId": 1,
+        "id": 1,
+        "title": request.form["title"],
+        "body": str(history[0][0])
     }
+
+
+def generate_image_from_base64(filename, string):
+    file = open('./{filename}.jpg'.format(filename=filename), 'wb')
+    file.write(base64.b64decode((string)))
+    file.close()
+
+    return './{filename}.jpg'.format(filename=filename)
+
+
+def get_filename(filename):
+    return './{filename}.jpg'.format(filename=filename)
 
 
 if __name__ == "__main__":
